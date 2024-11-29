2c07ff9279b1ec6adeafcfe6e3ebdc3ce6d95e8c
@u_limit_ <yurim@ajou.ac.kr>
2024-11-29 16:58:49 +0900

    feat(MainPage): 루트 화면 구현
    루트 화면 기본 기능을 구현했습니다. 세세한 기능 및 반응형은 추가하지 않은 상태입니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,48 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 const Home = async () => {
+  const router = useRouter();
   return (
-    <main className="container mx-auto min-h-screen max-w-[840px] p-6">
-      Web Playground Template
+    <main className="container mx-auto min-h-screen p-6 align-middle">
+      <p className={"font-bold"}>서비스 로고</p>
+      <div className="flex min-h-screen items-center justify-around">
+        <div
+          className={"flex flex-col justify-between"}
+          onClick={() => router.push("/chat")}
+        >
+          <div className="flex h-[465px] w-[465px] flex-col justify-center justify-items-center rounded-[40px] bg-[#FFC9C9]">
+            <div className="flex flex-col items-center">
+              <Image
+                src={"/main/santa.png"}
+                width={320}
+                height={329}
+                alt={"santa"}
+              />
+            </div>
+          </div>
+          <div className={"justify-items-center text-[35px]"}>
+            <p>Chat with Santa</p>
+            <p className={"font-bold"}>산타와 대화하기</p>
+          </div>
+        </div>
+        <div className={"flex flex-col justify-between"}>
+          <div className="flex h-[465px] w-[465px] flex-col justify-center justify-items-center rounded-[40px] bg-[#C9FFDE]">
+            <div className="flex flex-col items-center">
+              <Image
+                src={"/main/christmasDate.png"}
+                width={320}
+                height={329}
+                alt={"santa"}
+              />
+            </div>
+          </div>
+          <div className={"justify-items-center text-[35px]"}>
+            <p>Leaderboard</p>
+            <p className={"font-bold"}>순위 경쟁하기</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
