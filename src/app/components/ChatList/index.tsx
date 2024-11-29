2cd40a599b4f5aa933f833d696652ded7798137c
@u_limit_ <yurim@ajou.ac.kr>
2024-11-30 03:50:54 +0900

    feat: ChatList 추가
    채팅한 사용자의 목록을 보여주는 컴포넌트 레이아웃을 구현했습니다.

---
+++ src/app/components/ChatList/index.tsx
@@ -0,0 +1,11 @@
+export const ChatList = () => {
+  return (
+    <aside className={"h-real-screen z-30 w-full md:w-[196px]"}>
+      <div
+        className={
+          "h-full rounded-[30px] bg-[#FFC9C9] px-3 pb-32 md:border-r md:pb-20"
+        }
+      ></div>
+    </aside>
+  );
+};
