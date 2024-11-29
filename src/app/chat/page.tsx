673cacb51526ae092fcd36183139705d40e44f00
@u_limit_ <yurim@ajou.ac.kr>
2024-11-30 03:51:14 +0900

    refactor: ChatList, ChatRoom 컴포넌트화

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,39 +1,18 @@
 "use client";
-import { useState, useEffect } from "react";
 import { ChatRoom } from "@/app/components/ChatRoom";
-import { ChatBottom } from "@/app/components/ChatBottom";
+import { ChatList } from "@/app/components/ChatList";
 
 const Chat = () => {
   // comment 작성시
-  const [gift, setGift] = useState("");
-  const [prompt, setPrompt] = useState("");
 
   // comment 작성 성공 시 산타 말 반환 값
-  const [santaSays, setSantaSays] = useState("");
-
-  const [submittedGift, setSubmittedGift] = useState("");
-  const [submittedPrompt, setSubmittedPrompt] = useState("");
 
   return (
     <div
-      className={
-        "flex h-screen min-w-full flex-col justify-center bg-[#FBFFC9]"
-      }
+      className={"min-w-screen-xl mx-auto flex min-h-screen bg-[#FBFFC9] px-2"}
     >
-      <ChatRoom
-        submittedGift={submittedGift}
-        submittedPrompt={submittedPrompt}
-        santaSays={santaSays}
-      />
-      <ChatBottom
-        gift={gift}
-        setGift={setGift}
-        prompt={prompt}
-        setPrompt={setPrompt}
-        setSubmittedGift={setSubmittedGift}
-        setSubmittedPrompt={setSubmittedPrompt}
-        setSantaSays={setSantaSays}
-      />
+      <ChatList />
+      <ChatRoom />
     </div>
   );
 };
