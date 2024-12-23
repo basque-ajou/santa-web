83688ff86be5cff7fb4337f4bbf287a074eb407d
@u_limit_ <yurim@ajou.ac.kr>
2024-12-24 00:34:47 +0900

    refactor: 컴포넌트 수정

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,18 +1,43 @@
 "use client";
 import { ChatRoom } from "@/app/components/ChatRoom";
 import { ChatList } from "@/app/components/ChatList";
+import { ChatBottom } from "@/app/components/ChatBottom";
+import { useState } from "react";
+import { Top } from "@/app/components/ChatRoom/Top";
 
 const Chat = () => {
-  // comment 작성시
+  const [gift, setGift] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [submittedGift, setSubmittedGift] = useState("");
+  const [submittedPrompt, setSubmittedPrompt] = useState("");
+  const [santaSays, setSantaSays] = useState("");
 
-  // comment 작성 성공 시 산타 말 반환 값
+  const nickName = localStorage.getItem("nickName");
 
   return (
     <div
-      className={"min-w-screen-xl mx-auto flex min-h-screen bg-[#FBFFC9] px-2"}
+      className={
+        "min-w-screen-xl mx-auto flex min-h-screen flex-col items-center gap-4 bg-[#FBFFC9] px-10"
+      }
     >
-      <ChatList />
-      <ChatRoom />
+      <Top nickName={nickName} />
+
+      <ChatRoom
+        submittedGift={submittedGift}
+        setSubmittedGift={setSubmittedGift}
+        submittedPrompt={submittedPrompt}
+        santaSays={santaSays}
+        nickName={nickName}
+      />
+      <ChatBottom
+        gift={gift}
+        setGift={setGift}
+        prompt={prompt}
+        setPrompt={setPrompt}
+        setSubmittedGift={setSubmittedGift}
+        setSubmittedPrompt={setSubmittedPrompt}
+        setSantaSays={setSantaSays}
+      />
     </div>
   );
 };
