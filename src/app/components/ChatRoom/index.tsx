e0cfd8c0da439566d64707dbcf265de0cd90f468
@u_limit_ <yurim@ajou.ac.kr>
2024-12-24 00:34:05 +0900

    refactor: 컴포넌트 이동

--- src/app/components/ChatRoom/index.tsx
+++ src/app/components/ChatRoom/index.tsx
@@ -1,23 +1,53 @@
-import { ChatBottom } from "@/app/components/ChatBottom";
-import { useState } from "react";
-import { Top } from "@/app/components/ChatRoom/Top";
+import { useEffect } from "react";
 
-export const ChatRoom = () => {
-  const [gift, setGift] = useState("");
-  const [prompt, setPrompt] = useState("");
-  const [submittedGift, setSubmittedGift] = useState("");
-  const [submittedPrompt, setSubmittedPrompt] = useState("");
-  const [santaSays, setSantaSays] = useState("");
+type Props = {
+  submittedGift: string;
+  setSubmittedGift: React.Dispatch<React.SetStateAction<string>>;
+  submittedPrompt: string;
+  santaSays: string;
+  nickName: string | null;
+};
+
+export const ChatRoom = ({
+  submittedGift,
+  setSubmittedGift,
+  submittedPrompt,
+  santaSays,
+  nickName,
+}: Props) => {
+  const getChatList = async () => {
+    try {
+      const data = await fetch("http://localhost:3000/api/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: nickName,
+        }),
+        credentials: "include",
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (submittedGift && submittedPrompt) {
+      setSubmittedGift(submittedGift);
+    }
+  }, [submittedGift, submittedPrompt]);
+
+  useEffect(() => {
+    getChatList();
+  }, []);
+
   return (
-    <div className="flex h-[640px] w-[1360px] flex-col items-center transition-transform duration-500 md:z-20 md:pl-[40px]">
-      <Top />
-
+    <div className="flex h-[600px] w-full flex-col items-center transition-transform duration-500">
       <div
         className={
           "bg-gradient-custom h-full w-full rounded-[30px] border px-[44px] py-[54px]"
         }
       >
-        <div className="left flex w-[333px] flex-row justify-center rounded-[30px] bg-[#FCE1E1]">
+        <div className="left flex w-fit flex-row justify-center rounded-[30px] bg-[#FCE1E1]">
           <p className={"px-[30px] py-[21px]"}>
             허허허 선물을 받고 싶다고?
             <br />
@@ -28,7 +58,7 @@ export const ChatRoom = () => {
           <div className="flex w-full flex-row-reverse">
             <div
               className={
-                "mb-4 flex w-fit flex-row-reverse rounded-[30px] bg-[#E1FCE3] px-[30px] py-[21px]"
+                "mb-4 flex w-fit flex-row-reverse rounded-[30px] bg-[#E1FCE3] px-7 py-[21px]"
               }
             >
               <p>
@@ -39,22 +69,14 @@ export const ChatRoom = () => {
             </div>
           </div>
         )}
-        {santaSays.length > 0 && (
-          <p className="w-[394px] rounded-[30px] bg-[#FCE1E1] px-[30px] py-[21px]">
-            {santaSays}
-          </p>
-        )}
+        <div className={"w-full md:w-1/2"}>
+          {santaSays.length > 0 && (
+            <p className="w-fit rounded-[30px] bg-[#FCE1E1] px-[30px] py-[21px]">
+              {santaSays}
+            </p>
+          )}
+        </div>
       </div>
-
-      <ChatBottom
-        gift={gift}
-        setGift={setGift}
-        prompt={prompt}
-        setPrompt={setPrompt}
-        setSubmittedGift={setSubmittedGift}
-        setSubmittedPrompt={setSubmittedPrompt}
-        setSantaSays={setSantaSays}
-      />
     </div>
   );
 };
